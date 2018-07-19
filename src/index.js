65e304890922b85edb664f71aed00360180160f9
jullynobre <jullynobre1@gmail.com>
2018-07-18 23:02:27 -0300

    class Square to functional component

--- src/index.js
+++ src/index.js
@@ -2,19 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        { this.props.value }
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares
+    });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square 
+        value={ this.state.squares[i] } 
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
